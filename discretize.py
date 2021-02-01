0414705581e347ec870b9a02f368b3c4604aa4c9
davidgu13 <davidgu1312@gmail.com>
2021-02-01 16:35:09 +0200

    discretizing functions

--- discretize.py
+++ discretize.py
@@ -1,3 +1,28 @@
+import numpy as np
 
-def make_table(num_dims, num_buckets):
-    
+
+def make_table_and_buckets(num_buckets, ranges):
+    """
+    :param ranges: list of tuples [(start,end)]
+    :param num_buckets: number of buckets in each dimension
+    :return: table, buckets
+    """
+    table = np.zeros(np.array(num_buckets)+2)
+    buckets = [np.linspace(r[0],r[1],num_buckets[i]+1) for i,r in enumerate(ranges)]
+    return table, buckets
+
+def observation_to_bucket(obs, buckets):
+    assert len(obs)==len(buckets)
+    output = np.zeros(len(obs),dtype=int)
+    for i in range(len(obs)):
+        output[i] = int(np.digitize([obs[i]], buckets[i])[0])
+    return output
+
+
+# ranges = [(-1, 1), (0, 5)]
+# ranges2 = [(-0.1,1.1)]
+# num_buckets = [10, 10]
+# num_buckets2 = [2]
+# table, buckets = make_table_and_buckets(num_buckets2,ranges2)
+# print(buckets)
+# print(observation_to_bucket([-1.1], buckets))
--- main.py
+++ main.py
@@ -1,15 +1,40 @@
 import gym
 import matplotlib.pyplot as plt
+import numpy as np
+from sys import getsizeof
+from discretize import *
+
+Y_LIMIT = (0.0, 5.0)
+ANGLE_LIMIT = (-10.0, 10.0)
+ANGULAR_VELOCITY = (-5.0, 5.0)
+LIMITS = [(-1, 1), Y_LIMIT, (-5.0, 5.0), (-5.0, 5.0), ANGLE_LIMIT, ANGULAR_VELOCITY, (-0.1,1.1), (-0.1,1.1)]
+
+NUM_BUCKETS = [20, 20, 10, 10, 20, 20, 2, 2]
+table, buckets = make_table_and_buckets(NUM_BUCKETS, LIMITS)
+table += 2.0
+print(getsizeof(table))
+print(np.prod(NUM_BUCKETS))
+
+fig2 = plt.figure()
+
+ax1 = fig2.add_subplot(211)
+ax2 = fig2.add_subplot(212)
 
 env = gym.make('LunarLanderContinuous-v2')
 env.reset()
 cords = []
 for t in range(1000):
     action = env.action_space.sample()
-    print('Action: ', action)
+    action[1] = 1.0
+    print('Action: ', np.round(action,4))
     observation, reward, done, info = env.step(action)
-    cords.append((observation[0], observation[1], t))
-    print('Observation: ', observation, ' Reward: ', reward)
+
+    # if observation[1] >= Y_LIMIT:
+    #     done = True
+    #     reward = -100
+
+    cords.append((observation[4], observation[5], t))
+    print('Observation: ', np.round(observation,decimals=4), ' Reward: ', np.round(reward,4))
     if done:
         print(f'Episode finished after {t+1} timesteps')
         break
@@ -21,5 +46,10 @@ xs = [cord[0] for cord in cords]
 ys = [cord[1] for cord in cords]
 zs = [cord[2] for cord in cords]
 
-plt.scatter(xs, ys, zs=zs)
+ax1.scatter(zs, xs)
+# ax1.yaxis('x')
+ax2.scatter(zs, ys)
+# ax2.yaxis('y')
+
+
 plt.show()
